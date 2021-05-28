4e7b0baf7d1bb0dba49faa14541ba9c19983a459
Cristofer Martinez <cristoferm@gmail.com>
2021-05-28 12:37:02 -0700

    funny and safe-for-work message

---
+++ main.js
@@ -0,0 +1,14 @@
+var $entryBtn = document.querySelector('.entry-button');
+$entryBtn.addEventListener('click', handleSwap);
+var $allViews = document.querySelectorAll('.view');
+
+function handleSwap(event) {
+  if (!event.target.matches('.swap')) {
+    return;
+  }
+  var location = event.target.getAttribute('data-view');
+
+  for (var i = 0; i < )
+
+
+}
