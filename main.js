f9cd1e50308e312cceca065df72988ab5bdce9bc
Rocky Truong <rockytruong19@gmail.com>
2021-05-28 12:58:19 -0700

    too many dayssss.

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+var data = {
+  view: 'default',
+  days: {
+    {Monday: []
+    },
+    {Tuesday: []
+    },
+    {Wednesday: []
+    },
+    {Thursday: []
+    },
+    {Friday: []
+    },
+    {Saturday: []
+    },
+    {Sunday: []
+    }
+  },
+}
+
 var $entryBtn = document.querySelector('.entry-button');
 $entryBtn.addEventListener('click', handleSwap);
 var $allViews = document.querySelectorAll('.view');
@@ -7,8 +27,13 @@ function handleSwap(event) {
     return;
   }
   var location = event.target.getAttribute('data-view');
-
-  for (var i = 0; i < )
-
-
+  for (var i = 0; i < $allViews.length; i++) {
+    if (location === $allViews[i].getAttribute('data-view')) {
+      $allViews[i].classList.remove('hidden');
+    } else {
+      $allViews[i].classList.add('hidden');
+    }
+  }
 }
+
+function
